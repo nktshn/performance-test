52f2dc5e0f419162e7d5d1e74784a9a5a00cd7fe
Nikita Shishin <nshishin@insys.io>
2019-04-20 00:07:58 +0600

    describe() new output format

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ const mapModule = require('./src/map');
 /**
  * initial data
  */
-const arrayLength = 1000000;
+const arrayLength = 5000000;
 const numberArray = getNumberArray(arrayLength);
 const _map = mapModule.map;
 
@@ -15,25 +15,37 @@ const _map = mapModule.map;
  */
 const run = () => {
     console.log(`Data size: ${arrayLength}`);
-    describe('native map', [[].map], [e => e * 2], numberArray);
     describe('map', _map, [numberArray, e => e * 2], null);
 }
 
 const describe = (message, funcArray, funcArgs, ctx) => {
+    let bestResult = { implementation: null, time: null };
     console.log(`--- Function: ${message} ---`);
-    funcArray.forEach((func, index) => {
-        console.time(`#${index} timing: `.padEnd(40));
-        func.call(ctx, ...funcArgs);
-        console.timeEnd(`#${index} timing: `.padEnd(40));
+    funcArray.forEach((implementation, index) => {
+        console.log(`Implementation #${index}: ${implementation.description.toString().padEnd(60, ' -')}`);
+        let currentResult;
+        const startTime = Date.now();
+        implementation.func.call(ctx, ...funcArgs);
+        currentResult = Date.now() - startTime;
+        console.log(`time: ${currentResult.toString().padStart(70)}ms`);
+        console.log('\n'.padStart(79, ' -'));
+        if (!bestResult.time) {
+            bestResult = { implementation: index, time: currentResult }
+        } else {
+            bestResult = (bestResult.time > currentResult) ? {
+                implementation: index,
+                time: currentResult
+            } : bestResult;
+        }
     });
+    console.log(`The fastest implementation is #${bestResult.implementation} with time ${bestResult.time}ms`);
 }
 
+/**
+ * helpers
+ */
 function getNumberArray(length) {
-    let res = [];
-    for (let i = 0; i < length; i++) {
-        res[i] = i;
-    }
-    return res;
+    return Array(length).fill().map((e, i) => i);
 }
 
 module.exports = {
--- src/map.js
+++ src/map.js
@@ -1,22 +1,43 @@
 const map = [
-    (data, cb) => {
-        const l = data.length
-        let r = new Array(l);
-        let i = 0;
-        for (i; i < l; i++) {
-            r[i] = cb(data[i]);
-        }
-        return r;
+    {
+        func: (data, cb) => {
+            return data.map(cb);
+        }, description: 'native'
     },
-    (data, cb) => {
-        const l = data && data.length || 0;
-        const callback = cb || (e => e);
-        let r = new Array(l);
-        let i = 0;
-        for (i; i < l; i++) {
-            r[i] = callback(data[i]);
-        }
-        return r;
+    {
+        func: (data, cb) => {
+            const l = data.length
+            let r = new Array(l);
+            let i = 0;
+            for (i; i < l; i++) {
+                r[i] = cb(data[i]);
+            }
+            return r;
+        }, description: 'once refs to length, instant initialization'
+    },
+    {
+        func: (data, cb) => {
+            const l = data && data.length || 0;
+            const callback = cb || (e => e);
+            let r = new Array(l);
+            let i = 0;
+            for (i; i < l; i++) {
+                r[i] = callback(data[i]);
+            }
+            return r;
+        }, description: 'does null-checking matters?'
+    },
+    {
+        func: (data, cb) => {
+            const l = data && data.length || 0;
+            const callback = cb || (e => e);
+            let r = [];
+            let i = 0;
+            for (i; i < l; i++) {
+                r[i] = callback(data[i]);
+            }
+            return r;
+        }, description: 'empty array without length initialization'
     },
 ]
 
