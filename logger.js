789c23b87923bd6331c19ba88b0572e285ef13bd
Nikita Shishin <nshishin@insys.io>
2019-04-20 14:06:12 +0600

    logger improvements, different array sizes to test

--- logger.js
+++ logger.js
@@ -6,29 +6,34 @@ const mapModule = require('./src/map');
 /**
  * initial data
  */
-const arrayLength = 5000000;
-const numberArray = getNumberArray(arrayLength);
+const longArrayLength = 5000000;
+const shortArrayLength = 50000;
+const resultCheckLength = 15;
+const longNumberArray = getlongNumberArray(longArrayLength);
+const shortNumberArray = getlongNumberArray(shortArrayLength);
 const _map = mapModule.map;
 
 /**
  * logic
  */
 const run = () => {
-    console.log(`Data size: ${arrayLength}`);
-    describe('map', _map, [numberArray, e => e * 2], null);
+    describe('map', _map, [longNumberArray, e => e * 2], null);
+    describe('map', _map, [shortNumberArray, e => e * 2], null);
 }
 
 const describe = (message, funcArray, funcArgs, ctx) => {
     let bestResult = { implementation: null, time: null };
     console.log(`--- Function: ${message} ---`);
+    console.log(`Data size: ${funcArgs[0].length}`);
     funcArray.forEach((implementation, index) => {
-        console.log(`Implementation #${index}: ${implementation.description.toString().padEnd(60, ' -')}`);
+        console.log(`Implementation #${index}: ${implementation.description}`.padEnd(80, ' -'));
         let currentResult;
         const startTime = Date.now();
-        implementation.func.call(ctx, ...funcArgs);
+        const functionResult = implementation.func.call(ctx, ...funcArgs);
         currentResult = Date.now() - startTime;
-        console.log(`time: ${currentResult.toString().padStart(70)}ms`);
-        console.log('\n'.padStart(79, ' -'));
+        console.log(`time: ${currentResult.toString().padStart(70)} ms`);
+        console.log(`result check: ${functionResult.filter((e, i) => i < resultCheckLength)}`);
+        console.log('\n'.padStart(80, ' -'));
         if (!bestResult.time) {
             bestResult = { implementation: index, time: currentResult }
         } else {
@@ -38,14 +43,15 @@ const describe = (message, funcArray, funcArgs, ctx) => {
             } : bestResult;
         }
     });
-    console.log(`The fastest implementation is #${bestResult.implementation} with time ${bestResult.time}ms`);
+    console.log(`The fastest implementation is #${bestResult.implementation} with time ${bestResult.time} ms`);
 }
 
 /**
  * helpers
  */
-function getNumberArray(length) {
-    return Array(length).fill().map((e, i) => i);
+function getlongNumberArray(length, mapCb) {
+    let cb = mapCb || ((e, i) => i);
+    return Array(length).fill().map(cb);
 }
 
 module.exports = {
--- src/map.js
+++ src/map.js
@@ -31,6 +31,18 @@ const map = [
         func: (data, cb) => {
             const l = data && data.length || 0;
             const callback = cb || (e => e);
+            let r = (l > 64000 && new Array(l)) || [0];
+            let i = 0;
+            for (i; i < l; i++) {
+                r[i] = callback(data[i]);
+            }
+            return r;
+        }, description: 'no array initialization if length is not big'
+    },
+    {
+        func: (data, cb) => {
+            const l = data && data.length || 0;
+            const callback = cb || (e => e);
             let r = [];
             let i = 0;
             for (i; i < l; i++) {
@@ -38,6 +50,19 @@ const map = [
             }
             return r;
         }, description: 'empty array without length initialization'
+    },
+    {
+        func: (data, cb) => {
+            const l = data && data.length || 0;
+            const callback = cb || (e => e);
+            let r = new Array(l);
+            let i = 0;
+            while (i < l) {
+                r[i] = callback(data[i]);
+                i++;
+            }
+            return r;
+        }, description: 'while instead of for with length init'
     },
 ]
 
